093aaa415ff3668d7cf0952ba4d573b75844de11
hurfy <hurfydev@gmail.com>
2024-08-10 20:32:32 +0300

    fix: Code duplication

--- utils/RunDebug.bat
+++ utils/RunDebug.bat
@@ -1,6 +1,1 @@
-@echo off
-
-cd /d %USERPROFILE%\AppData\Local\DayZ
-del *.log *.RPT *.mdmp /q /s
-
 start /d "D:\SteamLibrary\steamapps\common\DayZ" DayZDiag_x64.exe -mod=P:\Builds\@EventsAPI;D:\SteamLibrary\steamapps\common\DayZ\!Workshop\@ZomBerry Admin Tools;D:\SteamLibrary\steamapps\common\DayZ\!Workshop\@CF -mission=D:\SteamLibrary\steamapps\common\DayZ\Missions\dayzOffline.ChernarusPlus -window -filePatching
